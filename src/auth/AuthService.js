ff3d7da1432355841a7a5f7eab353295df30c100
fllfl <rimu@fisherieslogistics.com>
2017-06-17 11:35:00 +1200

    fix silly bug

--- src/auth/AuthService.js
+++ src/auth/AuthService.js
@@ -10,16 +10,35 @@ export default class AuthService {
     this.login = this.login.bind(this);
     this.isLoggedIn = this.isLoggedIn.bind(this);
     this.getToken = this.getToken.bind(this);
+    this.setToken = this.setToken.bind(this);
     this.token = null;
     this.logout = this.logout.bind(this);
   }
 
+  setToken(auth){
+    return AsyncStorage.setItem(`auth-${this.clientId}`, JSON.stringify(auth));
+  }
+
   login(options = {}) {
-    const setToken = (auth) => AsyncStorage.setItem(`auth-${this.clientId}`, JSON.stringify(auth));
-    return new Promise(resolve => {
-      this.lock.show(options, (err, profile, token) => {
-        setToken({ profile, token }).then(() => resolve({ profile, token }));
-      });
+
+    const onLogin = (err, profile, token, resolve, reject) => {
+      if(err) {
+        this.setToken(null)
+        reject(err);
+      }
+      this.setToken({ profile, token }).then(() => resolve({ profile, token }));
+    };
+
+    const standardOptions =  {
+      closable: true,
+      scopes: 'offiline_access',
+      authParams: {
+        connection: 'Username-Password-Authentication',
+      },
+    }
+    return new Promise((resolve, reject) => {
+      this.lock.show(Object.assign({}, standardOptions, options),
+        (err, profile, token) => onLogin(err, profile, token, resolve, reject));
     });
   }
 
