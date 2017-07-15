d439df9f198823367368356f5987835ea4570e6a
fllfl <rimu@fisherieslogistics.com>
2017-07-15 22:03:41 +1200

    faaarr go auto whatever

--- android/app/src/main/java/com/salesapp/MainApplication.java
+++ android/app/src/main/java/com/salesapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.salesapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.cardio.RNCardIOPackage;
 import com.auth0.lock.react.LockReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCardIOPackage(),
             new LockReactPackage(),
             new VectorIconsPackage()
       );
